0c95755306e9a2051e2e821adffc4b40040c2a44
Hannes Preishuber <hannesp@ppedv.de>
2024-07-16 12:05:39 +0200

    FieldCssClassProvider Boostrap Fix

---
+++ BlazorTraining239711/Components/Pages/modul5/BoostrapErrorFixFieldCssClassProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorTraining239711.Components.Pages.modul5
+{
+    public class BoostrapErrorFixFieldCssClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+           bool isInvalid= editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return isInvalid?"is-invalid":"is-valid";
+        }
+    }
+}
