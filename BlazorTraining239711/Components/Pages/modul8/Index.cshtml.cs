09692c5bde4d079e62df2e92329285b0ad7c7f9c
Hannes Preishuber <hannesp@ppedv.de>
2024-07-16 16:13:30 +0200

    JS geklimpere

---
+++ BlazorTraining239711/Components/Pages/modul8/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTraining239711.Components.Pages.modul8
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
