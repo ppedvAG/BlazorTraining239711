af9f1b41ea740f824ae4de7cb5c37e8f8d83a3c5
Hannes Preishuber <hannesp@ppedv.de>
2024-07-16 11:56:49 +0200

    Formular Komponenten

---
+++ BlazorTraining239711/Components/Pages/modul5/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTraining239711.Components.Pages.modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="da muss was rein")]
+        [StringLength(10,ErrorMessage  ="zwischen 3 und 10 Zeichen",MinimumLength =3)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        public DateTime GebDat { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [Range(150,189,ErrorMessage ="zwischen 150 und 189 sonst kopf ab")]
+        public int Gross { get; set; }
+    }
+}
